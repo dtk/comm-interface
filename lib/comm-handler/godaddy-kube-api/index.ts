86b4a7b1c2b03a0ad446f66314ff8889fee60029
Vedad Fejzagic <vedad.fejzagic@atlantbh.com>
2019-12-09 13:41:09 +0100

    DTK-3931 add namespace param to watch method

--- lib/comm-handler/godaddy-kube-api/index.ts
+++ lib/comm-handler/godaddy-kube-api/index.ts
@@ -1,30 +1,34 @@
-const Client = require('kubernetes-client').Client
-const K8sConfig = require('kubernetes-client').config
+const Client = require('kubernetes-client').Client;
+const K8sConfig = require('kubernetes-client').config;
 
 export default class GoDaddyKubeApi {
-
   private client: any;
   private basePath: any;
   /**
-   * 
+   *
    * @param configMethod i.e: fromKubeconfig()
    * @param version i.e: 1.9
    */
-  constructor(endpoint: string, crdVersion: string, configMethod: string, clientVersion: string = '1.9') {
+  constructor(
+    endpoint: string,
+    crdVersion: string,
+    configMethod: string,
+    clientVersion: string = '1.9'
+  ) {
     const config = eval(`K8sConfig.${configMethod}`);
-    if(configMethod === 'getInCluster()') {
+    if (configMethod === 'getInCluster()') {
       this.client = new Client({ config: config });
       this.client.loadSpec();
-    }
-    else if(configMethod === 'fromKubeconfig()'){
+    } else if (configMethod === 'fromKubeconfig()') {
       this.client = new Client({ config: config, version: clientVersion });
-    }
-    else throw new Error('config method not recognized');
+    } else throw new Error('config method not recognized');
     this.basePath = `this.client.apis['${endpoint}'].${crdVersion}`;
   }
 
   async createCRD(body: string, kubeEndPoint: string = 'apiextensions.k8s.io') {
-    return await this.client.apis[kubeEndPoint].v1beta1.customresourcedefinitions.post({ body: body });
+    return await this.client.apis[
+      kubeEndPoint
+    ].v1beta1.customresourcedefinitions.post({ body: body });
   }
 
   async addCRD(body: any) {
@@ -35,31 +39,40 @@ export default class GoDaddyKubeApi {
     return await this.client.api.v1.namespaces.post({ body: body });
   }
 
-  async getBaseCRDInstancePath(namespace: string, plural: string, instanceName: string) {
-    return await eval(`${this.basePath}.namespace('${namespace}').${plural}('${instanceName}')`);
+  async getBaseCRDInstancePath(
+    namespace: string,
+    plural: string,
+    instanceName: string
+  ) {
+    return await eval(
+      `${this.basePath}.namespace('${namespace}').${plural}('${instanceName}')`
+    );
   }
 
   /**
-   * 
-   * @param body 
-   * @param namespace 
+   *
+   * @param body
+   * @param namespace
    * @param endpoint corresponds to 'group' property in crd
    * @param kind must be lowercase
    */
   async createCRDInstance(namespace: string, kind: string, body: any) {
-    return await eval(`${this.basePath}.namespace('${namespace}').${kind}.post( { body: body } )`);
+    return await eval(
+      `${this.basePath}.namespace('${namespace}').${kind}.post( { body: body } )`
+    );
   }
-  
-  async getCRDWatch(plural: string) {
-    return await eval(`${this.basePath}.watch.${plural}`);
+
+  async getCRDWatch(namespace: string, plural: string) {
+    return await eval(
+      `${this.basePath}.watch.namespace('${namespace}').${plural}`
+    );
   }
 
   async getSimplePath() {
     return this.basePath;
   }
 
-  getClient(){
+  getClient() {
     return this.client;
   }
-
 }
